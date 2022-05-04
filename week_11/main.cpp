3e6c428174488110cb191312fe0c18bd627f7d6e
Atanas Ivanov <naskuuu123@gmail.com>
2022-05-04 18:02:14 +0300

    add exercises and tasks from lab

--- week_11/main.cpp
+++ week_11/main.cpp
@@ -6,21 +6,52 @@ using namespace std;
 class A
 {
 public:
-  A()  { cout << "A's constructor called" << endl; }
+  A() { 
+    cout << "A's constructor called" << endl; 
+  }
+
+  void print() const {
+    cout << "Hello, I am A" << endl;
+  }
+
+  ~A() {
+    cout << "DESTRUCTOR A" << endl;
+  }
 };
   
 class B
 {
 public:
-  B()  { cout << "B's constructor called" << endl; }
+  B() { 
+    cout << "B's constructor called" << endl; 
+  }
+
+  void print() const {
+    cout << "Hello, I am B" << endl;
+  }
+
+  ~B() {
+    cout << "DESTRUCTOR B" << endl;
+  }
 };
   
-class C: public B, public A  // обърнете внимание на реда
+class C: public B, public A  // Note the order
 {
 public:
-  C()  { cout << "C's constructor called" << endl; }
+  C() { 
+    cout << "C's constructor called" << endl; 
+  }
+
+  ~C() {
+    cout << "DESTRUCTOR C" << endl;
+  }
+
+  void print () { 
+    A::print();
+    B::print();
+  }
 };
-  
+
 int main()
 {
     C c;
